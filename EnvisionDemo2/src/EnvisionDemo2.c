67199f233840a26d2e5ebd7885cc22d21a6962c7
john blaiklock <miniwinwm@gmail.com>
2019-03-19 12:11:57 +0000

    Added flash project

---
+++ EnvisionDemo2/src/EnvisionDemo2.c
@@ -0,0 +1,128 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <iodefine.h>
+
+void system_clock_config(void);
+void non_vol_load(uint8_t *data, uint16_t length);
+void non_vol_save(uint8_t *data, uint16_t length);
+
+typedef struct
+{
+	char test_text[45];
+	uint32_t test_numbers[10];
+} test_data_t;
+
+test_data_t test_data_1 = {{"Envision Kit GNURX flash data memory test"}, {0U, 1U, 2U, 3U, 4U, 5U, 6U, 7U, 8U, 9U}};
+test_data_t test_data_2;
+
+int main(void)
+{
+	bool success;
+	system_clock_config();
+
+	non_vol_save((uint8_t *)&test_data_1, sizeof(test_data_t));
+	non_vol_load((uint8_t *)&test_data_2, sizeof(test_data_t));
+
+	if (memcmp(&test_data_2, &test_data_1, sizeof(test_data_t)) == 0)
+	{
+		success = true;
+	}
+	else
+	{
+		success = false;
+	}
+
+	(void)success;
+
+    while (true)
+    {
+    }
+
+    return 0;
+}
+
+/**
+ * Configure the system and peripheral clocks
+ */
+void system_clock_config(void)
+{
+	/* enable all protect register bits */
+	SYSTEM.PRCR.WORD = 0xA50B;
+
+	/* forcibly oscillate the main clock */
+	SYSTEM.MOFCR.BIT.MOFXIN = 0U;
+
+	/* select resonator as main clock source */
+	SYSTEM.MOFCR.BIT.MOSEL = 0;
+
+	/* stop the HOCO, not using it */
+	SYSTEM.HOCOCR.BIT.HCSTP = 1U;
+
+	/* disable the sub-clock as no crystal input on hw */
+	RTC.RCR3.BIT.RTCEN = 0;
+
+	/* not using USB clock */
+	SYSTEM.SCKCR2.WORD = 0x0001U;
+
+	/* set drive level for 12MHz input */
+	SYSTEM.MOFCR.BIT.MODRV2 = 2U;
+
+	/* set wait time for starting main clock oscillator */
+	SYSTEM.MOSCWTCR.BYTE = 0x53;
+
+	/* start main clock oscillator */
+	SYSTEM.MOSCCR.BIT.MOSTP = 0U;
+
+	/* set rom wait state appropriate to main clock speed */
+	SYSTEM.ROMWT.BIT.ROMWT = 2U;
+
+	/* set PLL division to 1 */
+	SYSTEM.PLLCR.BIT.PLIDIV = 0U;
+
+	/* set PLL multiplication to 20 */
+	SYSTEM.PLLCR.BIT.STC = 39U;
+
+	/* start the PLL */
+	SYSTEM.PLLCR2.BIT.PLLEN = 0U;
+
+	/* wait for clock stabilization */
+	while (0 == SYSTEM.OSCOVFSR.BIT.PLOVF)
+	{
+		__asm("NOP");
+	}
+
+	/* ICLK to 120MHz */
+	SYSTEM.SCKCR.BIT.ICK = 1U;
+
+	/* FCLK to 60 MHz */
+	SYSTEM.SCKCR.BIT.FCK = 2U;
+
+	/* PCLKA to 120MHz */
+	SYSTEM.SCKCR.BIT.PCKA = 1U;
+
+	/* PCLKB to 60MHz */
+	SYSTEM.SCKCR.BIT.PCKB = 2U;
+
+	/* PCLKC to 60MHz */
+	SYSTEM.SCKCR.BIT.PCKC = 2U;
+
+	/* PCLKD to 60MHz */
+	SYSTEM.SCKCR.BIT.PCKD = 2U;
+
+	/* SDCLK not used */
+	SYSTEM.SCKCR.BIT.PSTOP0 = 1U;
+
+	/* BCLK not used */
+	SYSTEM.SCKCR.BIT.PSTOP1 = 1U;
+
+	/* switch main clock source to PLL */
+	SYSTEM.SCKCR3.BIT.CKSEL = 4U;
+
+	/* stop LOCO as finished with it now */
+	SYSTEM.LOCOCR.BYTE = 1U;
+
+	/* disable all protect register bits */
+	SYSTEM.PRCR.WORD = 0xA500;
+}
+
---
+++ EnvisionDemo2/src/non_vol.c
@@ -0,0 +1,107 @@
+#include <string.h>
+#include <stdint.h>
+#include <iodefine.h>
+
+#define FCU_COMMAND_AREA              	 	 	0x007E0000U
+#define DATA_FLASH_START_ADDRESS				0x00100000U
+
+typedef volatile uint8_t * FCU_BYTE_PTR __attribute__ ((aligned (4)));
+typedef volatile uint16_t * FCU_WORD_PTR __attribute__ ((aligned (4)));
+
+void non_vol_load(uint8_t *data, uint16_t length)
+{
+	(void)memcpy((data), ((uint8_t *)(uint32_t)DATA_FLASH_START_ADDRESS), ((size_t)length));
+}
+
+void non_vol_save(uint8_t *data, uint16_t length)
+{
+	const FCU_BYTE_PTR g_pfcu_cmd_area = (uint8_t*)FCU_COMMAND_AREA;
+	uint32_t blocks;
+	uint32_t block_counter;
+	uint32_t words;
+	uint32_t word_counter;
+	uint16_t short_word;
+
+	/* permits programming/erasure, and blank checking */
+	FLASH.FWEPROR.BIT.FLWE = 1;
+
+	/* set mode entry register with key to program/erase data memory */
+	FLASH.FENTRYR.WORD = 0xAA80;
+    while (FLASH.FENTRYR.WORD != 0x0080)
+    {
+    }
+
+    /* set erasure priority mode */
+    FLASH.FCPSR.WORD = 0x0001;
+
+    /* calculate number of 64 byte blocks to erase */
+	blocks = (uint32_t)length / 64U;
+	blocks += ((uint32_t)length % 64U > 0U) ? 1U : 0U;
+
+    /* do erase in 64 byte blocks */
+	for (block_counter = 0U; block_counter < blocks; block_counter++)
+	{
+		/* set command start address */
+		FLASH.FSADDR.LONG = DATA_FLASH_START_ADDRESS + block_counter * 64U;
+
+		/* send FACI command to do a block erase of 64 bytes of flash memory */
+		*g_pfcu_cmd_area = 0x20;
+		*g_pfcu_cmd_area = 0xd0;
+
+		/* wait until FACI command completed */
+		while (1 != FLASH.FSTATR.BIT.FRDY)
+		{
+		}
+	}
+
+    /* calculate number of 4 byte words to write */
+    words = (uint32_t)length / sizeof(uint32_t);
+   	words += ((uint32_t)length % sizeof(uint32_t) > 0U) ? 1U : 0U;
+
+   	/* write each 4 byte word */
+	for (word_counter = 0U; word_counter < words; word_counter++)
+	{
+		/* set command start address */
+		FLASH.FSADDR.LONG = DATA_FLASH_START_ADDRESS + word_counter * 4U;
+
+		*g_pfcu_cmd_area = 0xe8U;
+		*g_pfcu_cmd_area = 0x02U;
+
+		/* write first 2 byte short word */
+		short_word = (uint16_t)data[word_counter * 4U];
+		short_word += ((uint16_t)(data[word_counter * 4U + 1U])) << 8;
+		*(FCU_WORD_PTR) g_pfcu_cmd_area = short_word;
+
+		/* wait for buffer to empty */
+		while (FLASH.FSTATR.BIT.DBFULL == 1)
+		{
+		}
+
+		/* write second 2 byte short word */
+		short_word = (uint16_t)data[word_counter * 4U + 2U];
+		short_word += ((uint16_t)(data[word_counter * 4U + 3U])) << 8;
+		*(FCU_WORD_PTR) g_pfcu_cmd_area = short_word;
+
+		/* wait for buffer to empty */
+		while (FLASH.FSTATR.BIT.DBFULL == 1)
+		{
+		}
+
+		/* write command complete byte */
+		*g_pfcu_cmd_area = 0xd0U;
+
+		/* wait until FACI command completed */
+		while (1 != FLASH.FSTATR.BIT.FRDY)
+	    {
+	    }
+	}
+
+	/* reset Mode Entry Register with key */
+	FLASH.FENTRYR.WORD = 0xAA00;
+    while (FLASH.FENTRYR.WORD != 0x000)
+    {
+    }
+
+	/* prevents programming/erasure, and blank checking */
+	FLASH.FWEPROR.BIT.FLWE = 0;
+}
